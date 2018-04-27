433a0e5e40371fffe49692cb00ff940197a77341
charlieokey <charlesokeyokoro@gmail.com>
2018-04-27 14:25:48 -0700

    Added GET/ to test

--- server/server.js
+++ server/server.js
@@ -24,11 +24,11 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.status(200).send({todos});
     }, (e) => {
         res.status(400).send(e);
-    })
-})
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: "First test todo"
+}, {
+    text: 'Second test todo'
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
-})
+    Todo.remove({}).then(() => {
+         return Todo.insertMany(todos);
+    }).then(() => done());
+});
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
@@ -24,7 +32,7 @@ describe('POST /todos', () => {
                 return done(err);
             }
             
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -40,9 +48,20 @@ describe('POST /todos', () => {
         .end((err, res) => {
             if (err) return done(err);
             Todo.find().then((todos) => {
-                expect(todos.length).toEqual(0);
+                expect(todos.length).toEqual(2);
                 done();
             }).catch((e) => done(err));
         });
+    });
+});
+
+describe('GET/ todos', () => {
+    it('should return some todos', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2);
+        }).end(done);
     });
 });
